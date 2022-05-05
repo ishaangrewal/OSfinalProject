dddaf22becec1a4c574c99d4f76f585539a1b3e8
Nicole Zakiyan <nicolez@cs.utexas.edu>
2022-05-04 23:25:46 -0500

    keyboard

--- kernel/keyboard.cc
+++ kernel/keyboard.cc
@@ -3,10 +3,13 @@
 #include "debug.h"
 #include "machine.h"
 #include "smp.h"
-#include "keyboard.h"
+#include "idt.h"
 
 extern uint32_t keyBoardHandler_;
-uint32_t valueFromPort;
+uint32_t readFromCommandPort;
+uint32_t inputBit;
+uint32_t outputBit;
+//uint32_t valueFromPort;
 
 void KeyBoard::init(void) {
     //port, byte 
@@ -14,12 +17,17 @@ void KeyBoard::init(void) {
     //map scan code
     //outb writes one byte to port 
     //inb reads one byte from port
-    //inb(0x21, 0xfd);
-    //valueFromPort = inb(0x21);
-    Debug::printf("*** in keyboard init!\n");
-    outb(0x21, 0xfd);
-    //valueFromPort = inb(0x60);
-    IDT::interrupt(9, (uint32_t)&keyBoardHandler_);
+    // inb(0x21, 0x64);
+    // //valueFromPort = inb(0x21);
+    // readFromCommandPort = inb(0x64);
+    // inputBit = *(uint8_t *)&readFromCommandPort[1] & 0x0;
+    // outb(0x21, 0x64);
+    // outputBit = *(uint8_t *)&readFromCommandPort[0] & 0x1;
+    // valueFromPort = inb(0x60);
+
+    Debug::printf("interrupt scan code: %c", (char) valueFromPort);
+    //outb(0x20, 0x20);
+    IDT::interrupt(9,(uint32_t)&keyBoardHandler_);
 }
 //registers as interrupt in interrupt handler
 //check status of keyboard if changed, read in info and display
@@ -27,14 +35,24 @@ void KeyBoard::init(void) {
 //use inb to read 0x64 see if its changed 
 //call init in kernel or config 
 extern "C" void keyBoardHandler() {
-    Debug::printf("*** in keyboard!\n");
-    return;
+    readFromCommandPort = inb(0x64);
+    inputBit = *(uint8_t *)&readFromCommandPort[1] & 0x0;
+    outb(0x21, 0x64);
+    outputBit = *(uint8_t *)&readFromCommandPort[0] & 0x1;
+    valueFromPort = inb(0x60);
+    uint32_t letter = scanCodeToLetter(valueFromPort);
+
+
+
+
+    // Debug::printf("interrupt scan code: %c", (char) inb(0x60));
+    // outb(0x20, 0x20);
     int full = inb(0x64) & 0x1;
-    if (full) {
-        int clearInputBuffer = inb(0x64) & 0x10;
-        if (!clearInputBuffer) {
-            outb(0x60,0x21);
-        }
-    }
+    // if (full) {
+    //     int clearInputBuffer = inb(0x64) & 0x10;
+    //     if (!clearInputBuffer) {
+    //         outb(0x60,0x21);
+    //     }
+    // }
 }
 
